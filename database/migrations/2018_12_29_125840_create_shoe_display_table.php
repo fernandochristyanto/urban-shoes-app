39dc4f8793af3368a38bdafcf94ef974ce04482f
RusticKey <rustickey@hotmail.com>
2018-12-29 20:26:16 +0700

    Initial commit, added migrations

---
+++ database/migrations/2018_12_29_125840_create_shoe_display_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateShoeDisplayTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shoe_display', function (Blueprint $table) {
+            $table->increments('id');
+            $table->id('shoe_id')->unsigned();
+            $table->id('article_id')->unsigned();
+            $table->char('stsrc', 1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shoe_display');
+    }
+}
