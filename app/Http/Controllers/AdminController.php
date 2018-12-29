3a2d20733ccadfa1f30128f0701776bc7772907c
Fernando Christyanto <christyantofernando@gmail.com>
2018-12-29 21:15:16 +0700

    add empty admin views

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -10,4 +10,28 @@ class AdminController extends Controller
     public function home(){
         return view('admin.home');
     }
+
+    public function discover(){
+        return view('admin.discover');
+    }
+
+    public function news(){
+        return view('admin.news');
+    }
+
+    public function search(){
+        return view('admin.search');
+    }
+
+    public function completedItems(){
+        return view('admin.admin-panel.completedItems');
+    }
+
+    public function pendingItems(){
+        return view('admin.admin-panel.pendingItems');
+    }
+
+    public function requestNewItem(){
+        return view('admin.admin-panel.requestNewItem');
+    }
 }
